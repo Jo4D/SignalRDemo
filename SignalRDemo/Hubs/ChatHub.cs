fdd75ba9c74cf7eb4834696cb0dc1527ef7cae53
Jo4D <yusufhasan182@gmail.com>
2022-12-28 16:24:13 +0200

    signalr implementation

---
+++ SignalRDemo/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRDemo
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string fromuser,string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", fromuser, message);
+        }
+    }
+}
---
+++ SignalRDemo/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("chatHub").build();
+
+connection.on("ReceiveMessage", function (fromuser, message) {
+    var msg = fromuser + " : " + message
+    var li = document.createElement("li");
+    li.textContent = msg;
+    $("#list").prepend(li);
+});
+
+connection.start();
+
+$("#btnsend").on("click", function () {
+    var fromuser = $("#txtuser").val();
+    var message = $("#txtmessge").val();
+
+    connection.invoke("SendMessage", fromuser, message);
+})
